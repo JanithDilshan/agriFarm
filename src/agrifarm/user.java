d24a5e11d5070dbdeafb3694dfa0c04e7cc1d806
Dilshan <Dilshan@DESKTOP-Q00E5TT>
2017-08-04 13:49:21 +0530

    new commit

--- src/agrifarm/user.java
+++ src/agrifarm/user.java
@@ -4,11 +4,40 @@
  * and open the template in the editor.
  */
 package agrifarm;
+import java.io.IOException;
+import java.io.Serializable;
 
 /**
  *
  * @author Dilshan
  */
-public class user {
+public class user implements Serializable {
+    
+    private String username;
+    private String password;
+    
+    
+     public user(String username, String password){
+        
+       this.username = username;
+       this.password = password;
+    } 
+    
+    public void register(){
+        
+        
+     
+    }
+    
+     public String getUsername(){
+      return username;
+  }
+     public String getPassword(){
+      return password;
+  }
+    
+    
     
 }
+
+
